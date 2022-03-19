02103feb4b8292efb80e5ec60f639b4aaafd9287
JustTory <johnnynathany@gmail.com>
2022-03-20 00:07:32 +0700

    import icons and colors from prototype

---
+++ src/constant/color.js
@@ -0,0 +1,26 @@
+const color = {
+    background: '#F7F8FA',
+
+    white: "#FFFFFF",
+
+    black: "#000000",
+
+    red: '#FF5454',
+
+    orange: '#FF9457',
+
+    green: '#04D000',
+    lightGreen: '#E9FFE7',
+
+    yellow: '#FFC700',
+    lightYellow: '#FFF4CD',
+
+    blue: '#3C72FF',
+    lightBlue: '#E3EEFF',
+
+    graySelected: "#F7F7F7",
+    grayFocused: '#ADAEAF',
+    grayText: '#868686',
+};
+
+export default color;
---
+++ src/constant/icon.js
@@ -0,0 +1,53 @@
+// common
+import DropDownIc from '../assets/ic_dropdown.svg'
+import GreyDropDownIc from '../assets/ic_grey_dropdown.svg'
+
+// navbar
+import DefaultAvaIc from '../assets/ic_default_avatar.svg'
+import HomeIc from '../assets/ic_home.svg'
+import SaleIc from '../assets/ic_sale.svg'
+import ProductIc from '../assets/ic_product.svg'
+import StatIc from '../assets/ic_statistic.svg'
+import OrderIc from '../assets/ic_order.svg'
+import VersionIc from '../assets/ic_version.svg'
+import LogOutIc from '../assets/ic_logout.svg'
+
+// home
+import TotalSalesIc from '../assets/ic_total_sales.svg'
+import TotalOrdersIc from '../assets/ic_total_orders.svg'
+import TotalUsersIc from '../assets/ic_total_users.svg'
+import YellowArrIc from '../assets/ic_yellow_arrow.svg'
+import GreenArrIc from '../assets/ic_green_arrow.svg'
+import BlueArrIc from '../assets/ic_blue_arrow.svg'
+import GreyArrIc from '../assets/ic_grey_arrow.svg'
+
+// sales
+import EyeIc from '../assets/ic_eye.svg'
+import GreenUpArrIc from '../assets/ic_green_up_arrow.svg'
+import RedDownArrIc from '../assets/ic_red_down_arrow.svg'
+import GreenUpStonkIc from '../assets/ic_green_up_stonk.svg'
+import RedDownStonkIc from '../assets/ic_red_down_stonk.svg'
+
+// product
+import SearchIc from '../assets/ic_search.svg'
+import FilterIc from '../assets/ic_filter.svg'
+import LeftArrowIc from '../assets/ic_left_arrow.svg'
+import RightArrowIc from '../assets/ic_right_arrow.svg'
+import PlusIc from '../assets/ic_plus.svg'
+import ThreeDotIc from '../assets/ic_three_dot.svg'
+
+// order
+import RecDropIc from '../assets/ic_received_dropdown.svg'
+import ProcDropIc from '../assets/ic_processing_dropdown.svg'
+import ShipDropIc from '../assets/ic_shipping_dropdown.svg'
+import CompDropIc from '../assets/ic_completed_dropdown.svg'
+import CanDropIc from '../assets/ic_cancelled_dropdown.svg'
+
+export {
+    DropDownIc, GreyDropDownIc,
+    DefaultAvaIc, HomeIc, SaleIc, ProductIc, StatIc, OrderIc, VersionIc, LogOutIc,
+    TotalSalesIc, TotalOrdersIc, TotalUsersIc, YellowArrIc, GreenArrIc, BlueArrIc, GreyArrIc,
+    EyeIc, GreenUpArrIc, RedDownArrIc, GreenUpStonkIc, RedDownStonkIc,
+    SearchIc, FilterIc, LeftArrowIc, RightArrowIc, PlusIc, ThreeDotIc,
+    RecDropIc, ProcDropIc, ShipDropIc, CompDropIc, CanDropIc
+}
