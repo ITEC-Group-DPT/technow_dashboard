388c53687812a0c6a4ae39f74ede49ee43e2450e
kurozemi <tranngminhdao@gmail.com>
2022-03-20 18:53:41 +0700

    pagination component

---
+++ src/components/Pagination/pagination.js
@@ -0,0 +1,84 @@
+import React from 'react'
+
+import { Box } from '@mui/system';
+import Button from '@mui/base/ButtonUnstyled';
+
+
+import { LeftArrowIc, RightArrowIc } from "../../constant/icon";
+import { Typography } from '@mui/material';
+import color from '../../constant/color';
+
+const Pagination = ({
+    page,
+    maxPages,
+
+    onBack,
+    onForward,
+}) => {
+    return (
+        <Box sx={styles.container}>
+
+            {/* back arrow */}
+            <Button
+                onClick = {onBack}
+                style={styles.arrowBox}
+            >
+                <img src={LeftArrowIc} width={14} height={14} />
+            </Button>
+
+            {/* forward arrow */}
+            <Button
+                onClick = {onForward}
+                style={styles.arrowBox}
+            >
+                <img src={RightArrowIc} width={14} height={14} />
+            </Button>
+
+            {/* pages box */}
+            <Box sx={styles.pageBox}>
+                <Typography
+                    sx={styles.pageText}
+                >
+                    Page {page}/{maxPages}
+                </Typography>
+            </Box>
+
+
+        </Box>
+    )
+}
+
+const styles = {
+    container: {
+        display: "flex",
+    },
+    arrowBox: {
+        border: "1px solid #D0D0D0",
+        background: "white",
+
+        borderRadius: "5px",
+        display: "flex",
+        alignItems: "center",
+
+        marginRight: "6px",
+        py: "10px",
+        px: "9px",
+    },
+    pageBox: {
+        border: "1px solid #D0D0D0",
+        background: "white",
+
+        borderRadius: "5px",
+        display: "flex",
+        alignItems: "center",
+
+        py: "10px",
+        px: "16px",
+    },
+    pageText: {
+        fontSize: 14,
+        color: color.lightGrayText,
+    },
+}
+
+export default Pagination;
--- src/demo/others.js
+++ src/demo/others.js
@@ -9,22 +9,29 @@ import color from '../constant/color';
 //component
 import SortByTime from "../components/SortByTime/sortByTime"
 import SearchBar from '../components/SearchBar/searchBar';
+import Pagination from '../components/Pagination/pagination';
 
 const OtherDemo = () => {
 
     const [sortValue, setSortValue] = useState("Month");
     const [search, setSearch] = useState("");
 
+    const [page, setPage] = useState(1);
+    const maxPages = 3;
+
     return (
         <Box
             sx={styles.sortContainer}
         >
+            {/* sort by time demo */}
             <SortByTime
                 onChangeValue={value => setSortValue(value)}
             />
 
             <Typography><b>Choose Sort:</b> {sortValue}</Typography>
             <br />
+
+            {/* search demo */}
 
             <Box sx={styles.searchContainer}>
                 <SearchBar
@@ -35,6 +42,29 @@ const OtherDemo = () => {
             </Box>
 
             <Typography><b>Search Text:</b> {search}</Typography>
+
+            <br />
+            {/* pagination demo */}
+
+            <Pagination
+                page={page}
+
+                // default maxPages = 3
+                maxPages={maxPages}
+
+                onBack={() => {
+                    if (page > 1)
+                        setPage(page - 1);
+                }}
+
+                onForward={() => {
+                    if (page < maxPages)
+                        setPage(page + 1);
+                }}
+            />
+
+            <br />
+            <Typography><b>Current Page:</b> {page}</Typography>
 
         </Box>
     )
@@ -50,11 +80,6 @@ const styles = {
         alignItems: "center",
     },
     searchContainer: {
-        background: "white",
-        display: "flex",
-        flexDirection: "column",
-
-        py: 1,
         px: 1,
 
         mb: 1,
