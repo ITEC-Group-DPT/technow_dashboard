bac856569ae3d75f298414f472c0d32818fd194e
kurozemi <tranngminhdao@gmail.com>
2022-04-07 19:55:40 +0700

    update customer row padding

--- src/pages/Customers/customer.style.js
+++ src/pages/Customers/customer.style.js
@@ -9,10 +9,12 @@ const Style = {
         marginBottom: "1rem"
     },
 
+
     titleWrapper: {
         display: "flex",
         justifyContent: "space-between"
     },
+
 
     chart: {
         marginBottom: "2rem",
@@ -20,7 +22,9 @@ const Style = {
         borderRadius: "15px",
         paddingLeft: "45px",
         py: "16px",
+
     },
+
 
     chartTitle: {
         fontSize: "22px",
@@ -31,15 +35,60 @@ const Style = {
     },
 
 
+
     //leaderboard
-    leaderboard: { display: "block", background: color.white, paddingTop: "20px", borderRadius: '15px', minHeight: "93vh", position: "relative" },
-    boardTitle: { paddingLeft: "50px", paddingTop: "20px", paddingBottom: "20px" },
-    boardRow: { paddingLeft: "60px", paddingTop: "15px", paddingBottom: "10px"},
-    paginationBox: { paddingTop: '20px', paddingBottom: '10px', position: 'absolute', bottom: "10px", left: "200px" },
-    firstRank: { color: color.gold, background: color.lightGold, padding: '3px 5px 3px 5px', borderRadius: '5px' },
-    secondRank: { color: color.silver, background: color.lightSilver, padding: '3px 5px 3px 5px', borderRadius: '5px' },
-    thirdRank: { color: color.bronze, background: color.lightBronze, padding: '3px 5px 3px 5px', borderRadius: '5px' },
-    noRank: { padding: '3px 5px 3px 5px' }
+    leaderboard: {
+        display: "block",
+        background: color.white,
+        paddingTop: "20px",
+        borderRadius: '15px',
+        minHeight: "88vh",
+        position: "relative"
+    },
+
+    boardTitle: {
+        paddingLeft: "50px",
+        paddingTop: "20px",
+        paddingBottom: "10px"
+    },
+
+    boardRow: {
+        paddingLeft: "60px",
+        paddingTop: "20px",
+    },
+
+    paginationBox: {
+        paddingTop: '20px',
+        paddingBottom: '10px',
+        position: 'absolute',
+        bottom: "10px",
+        left: "200px"
+    },
+
+    firstRank: {
+        color: color.gold,
+        background: color.lightGold,
+        padding: '3px 10px',
+        borderRadius: '5px'
+    },
+
+    secondRank: {
+        color: color.silver,
+        background: color.lightSilver,
+        padding: '3px 10px',
+        borderRadius: '5px'
+    },
+
+    thirdRank: {
+        color: color.bronze,
+        background: color.lightBronze,
+        padding: '3px 10px',
+        borderRadius: '5px'
+    },
+
+    noRank: {
+        padding: '3px 10px',
+    }
 };
 
 export default Style;
